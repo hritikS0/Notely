1fe58117706ba81a1e53ade5fcca6df4b8cf7042
hritikS0 <sharmahritik8077@gmail.com>
2026-04-02 20:46:50 +0530

    changes for render

--- api/src/app.js
+++ api/src/app.js
@@ -2,9 +2,20 @@ import express from "express";
 import cors from "cors";
 import { connectDb } from "./config/db.js";
 import rootRouter from "./routes/routes.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-app.use(cors());
+const allowedOrigins = process.env.CLIENT_URL
+  ? process.env.CLIENT_URL.split(",").map((o) => o.trim())
+  : ["http://localhost:5173"];
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api", rootRouter);
--- api/src/index.js
+++ api/src/index.js
@@ -1,5 +1,4 @@
-import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import app from "./app.js";
 
 
