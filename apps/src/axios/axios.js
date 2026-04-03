098b0d585497510fcbcc64e936e691c2f14eb11b
hritikS0 <sharmahritik8077@gmail.com>
2026-04-03 12:42:39 +0530

    Increased limit of axios timeout to 3000

--- apps/src/axios/axios.js
+++ apps/src/axios/axios.js
@@ -5,7 +5,7 @@ const baseURL =
 
 const axiosInstance = axios.create({
   baseURL,
-  timeout: 10000,
+  timeout: 30000,
   headers: { "X-Custom-Header": "foobar" },
 });
 axiosInstance.interceptors.request.use((config) => {
